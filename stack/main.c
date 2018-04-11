4cbab9501f0d4381030c5a4abad61fa96194fdb7
hliu <username@users.noreply.github.com>
2018-04-10 19:04:45 -0600

    finish stack

--- stack/main.c
+++ stack/main.c
@@ -3,5 +3,47 @@
 
 int main()
 {
+    {
+        STACK *s = stack_create(sizeof(int));
+        int y, i;
+
+        printf("------------- test, int -------------\n");
+        for (i = 0; i < 20; i++) {
+            stack_push(s, &i);
+            printf("pushed int: %d\n", i);
+        }
+
+        printf("\n");
+
+        for (i = 0; i < 20; i++) {
+            stack_pop(s, &y);
+            printf("poped  int: %d\n", y);
+        }
+
+        stack_destroy(s);
+    }
+
+    {
+        STACK *s = stack_create(sizeof(double));
+        double y;
+        int i;
+
+        printf("\n------------- test, double -------------\n");
+        for (i = 0; i < 20; i++) {
+            y = i * i;
+            stack_push(s, &y);
+            printf("pushed double: %g\n", y);
+        }
+
+        printf("\n");
+
+        for (i = 0; i < 20; i++) {
+            stack_pop(s, &y);
+            printf("poped  double: %g\n", y);
+        }
+
+        stack_destroy(s);
+    }
+
     return 0;
 }
